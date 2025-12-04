6b566979b84c81b1263e9ad7ce38e00c26970951
kkkkk-a <151097611+kkkkk-a@users.noreply.github.com>
2025-12-04 23:40:22 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'media-saver-cache-v3'; // Cache version updated
+const URLS_TO_CACHE = [
+  '.',
+  'index.html',
+  'favicon.webp', // このファイルのみをキャッシュ
+  'https://cdnjs.cloudflare.com/ajax/libs/Sortable/1.15.0/Sortable.min.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(URLS_TO_CACHE);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// 古いキャッシュを削除
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
